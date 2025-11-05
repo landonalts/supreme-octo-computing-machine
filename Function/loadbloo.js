1edd42a4405a68535cccf894370670ca6123217e
landonalts <1630306@sweetwaterschools.net>
2025-11-04 21:44:10 -0800

    Update loadbloo.js

--- Function/loadbloo.js
+++ Function/loadbloo.js
@@ -1,1 +1,14 @@
+// functions/loadBloo.js
+import fs from 'fs';
+import path from 'path';
 
+export async function handler() {
+  const filePath = path.join(process.cwd(), 'Bloo.js');
+  const code = fs.readFileSync(filePath, 'utf8');
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/javascript' },
+    body: code
+  };
+}
